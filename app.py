c7857ae90349221b425cf74ef2367edce5a6beac
PoojithaValluri <149654727+PoojithaValluri@users.noreply.github.com>
2023-11-03 13:00:07 +0000

    added html css

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'python Developer',
+        'location': 'Banglore',
+        'salary': '10,00,000',
+    },
+    {
+        'id': 2,
+        'title': 'java Developer',
+        'location': 'Hyd',
+        'salary': '12,00,000',
+    },
+    {
+        'id': 3,
+        'title': 'mulesoft',
+        'location': 'Hyd',
+    },
+]
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello Poojitha</p>"
+  return render_template('home.html', jobs=JOBS)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+  app.run(host="0.0.0.0", debug=True)
